a8af82fb431f9a0296b0b1d97d264660017b0b64
Myrausman <maira.usman5703@gmail.com>
2023-06-14 21:33:04 +0500

    .

--- website/views.py
+++ website/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render,redirect,get_object_or_404
-
+import random
+from database.models import *
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+            login = Login.objects.filter(email=request.POST.get('email')).first()
+            return render(request, 'index.html', {'email':login.email,'login':True})
     return render(request, 'index.html', {})
-def ask_view(request):
-    return render(request, 'ask.html', {})
+def ask_view(request,email):
+    return render(request, 'ask.html', {"email":email})
 def mytopics_view(request):
-    return render(request, 'mytopics.html', {})
+    
+    return render(request, 'mytopics.html')
 def register(request):
     return render (request,'register.html',{})
 def details(request):
+    if request.method=='POST':
+         questionTitle=request.POST['questionTitle']
+         questionDetails=request.POST['questionDetails']
+         tags=request.POST['tags']
+         question=Question.objects.create(
+              title=questionTitle,
+              details=questionDetails,
+              tag=tags
+         )
+         question.save()
+         return redirect('/details')
+
     return render (request,'details.html',{})
