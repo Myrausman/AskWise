48ca80dd028876d2c437dd29aeb04a3547dcb874
Myrausman <maira.usman5703@gmail.com>
2023-06-15 23:48:09 +0500

    .

--- website/views.py
+++ website/views.py
@@ -27,8 +27,35 @@ def mytopics_view(request):
     return render(request, 'mytopics.html')
 def login(request):
     return render (request,'login.html',{})
+
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+import sqlite3
+import random
+import string
+
+@csrf_exempt
 def register(request):
+    if request.method == 'POST':
+        # Retrieve the form data
+        first_name = request.POST.get('fname')
+        last_name = request.POST.get('lname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        gender = request.POST.get('gender')
+
+
+        # Insert the user data into the database
+        with sqlite3.connect('datbase.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO users ( fname, lname, email, password, gender) VALUES ( ?, ?, ?, ?, ?)",
+                           ( first_name, last_name, email, password, gender))
+
+        return redirect('home')
+
     return render (request,'register.html',{})
+
+
 def details(request):
     if request.method=='POST':
          questionTitle=request.POST['questionTitle']
